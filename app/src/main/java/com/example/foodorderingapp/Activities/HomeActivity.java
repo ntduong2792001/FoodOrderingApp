c638157740a62aa570d684e012622eab78c46976
Anh Tuan <tuanlnahe150485@fpt.edu.vn>
2022-07-19 21:09:27 +0700

    HomeActivity

--- app/src/main/java/com/example/foodorderingapp/Activities/HomeActivity.java
+++ app/src/main/java/com/example/foodorderingapp/Activities/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.foodorderingapp.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 
 import com.example.foodorderingapp.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.squareup.picasso.Picasso;
 
 public class HomeActivity extends AppCompatActivity {
     Button btnUser;
@@ -29,6 +31,11 @@ public class HomeActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
          }
+        else{
+            tvGreet.setText("Hello, "+ auth.getCurrentUser().getDisplayName());
+            Uri photo = auth.getCurrentUser().getPhotoUrl();
+            Picasso.with(this).load(photo).into(avatar);
+        }
 
 
 
